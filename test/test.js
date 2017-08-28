019d07c30dde4244d5af6cc3db2806f8d82b0ca5
Justin Bull <bull.justin@gmail.com>
2017-08-28 14:26:04 -0700

    Move javascript utils into class and add a few tests

--- test/test.js
+++ test/test.js
@@ -3,10 +3,27 @@ const mocha = require('mocha');
 const describe = mocha.describe;
 const it = mocha.it;
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
-    });
+// import Utils from "../assets/src/scripts/classes/class.utils.js";
+const Utils = require("../assets/src/scripts/classes/class.utils.js");
+
+describe('Utils.serialize', () => {
+
+  it('should return an empty string when an unexpected parameter is given', function () {
+    assert.equal("", Utils.serialize(0));
+    assert.equal("", Utils.serialize(true));
+    assert.equal("", Utils.serialize(null));
   });
+
+  it('should return an empty string when an empty object is given', function () {
+    assert.equal("", Utils.serialize({}));
+  });
+
+  it('should return a serialize string when an object is given', function () {
+    assert.equal("hello=world&num=123", Utils.serialize({hello: 'world', num: 123}));
+  });
+
+  it('should break apart characters when a string is given', function () {
+    assert.equal("0=a&1=b&2=c", Utils.serialize('abc'));
+  });
+
 });
