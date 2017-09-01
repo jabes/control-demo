ea319d29364cc2b16a8b23e0e8d0ae28be3b8ccc
Justin Bull <bull.justin@gmail.com>
2017-09-01 10:51:28 -0700

    Add header to dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,36 +1,42 @@
 <template>
-  <div class="container">
+  <div>
 
-    <div class="flex items-center">
-      <div class="flex-auto">
-        <h1>Dashboard</h1>
+    <header class="relative margin-bottom-300">
+      <div class="absolute index-0 top-0 bottom-0 left-0 right-0 width-100 height-100 bg-white opacity-10"></div>
+      <div class="relative index-10 container">
+        <div class="flex items-center">
+          <div class="flex-auto">
+            <h1>Dashboard</h1>
+          </div>
+          <div class="flex-none">
+            <router-link :to="{ name: 'logout' }">Logout</router-link>
+          </div>
+        </div>
       </div>
-      <div class="flex-none">
-        <router-link :to="{ name: 'logout' }">Logout</router-link>
-      </div>
-    </div>
+    </header>
 
-    <form class="margin-top-200 margin-bottom-300"
-          v-on:submit.prevent>
-      <input type="text"
-             placeholder="Enter your reminder.."
-             v-model="todo.message"
-             required>
-      <button class="btn secondary-white" v-on:click="add(todo.message)">Add</button>
-    </form>
-
-    <ul class="list-style-none margin-0 padding-0">
-      <li class="flex items-center margin-bottom-100"
-          v-for="todo in todos"
-          v-bind:class="{ 'opacity-50': todo.completed }">
-        <input type="checkbox"
-               v-bind:checked="todo.completed"
-               v-on:change="toggle(todo.id, todo.completed)">
-        <span class="font-size-18 margin-left-100 margin-right-100"
-              v-bind:class="{ 'text-decoration-line-through': todo.completed }">{{ todo.message }}</span>
-        <button class="trash" v-on:click="remove(todo.id)" v-html="icons.trash"></button>
-      </li>
-    </ul>
+    <article class="container">
+      <form class="margin-bottom-300"
+            v-on:submit.prevent>
+        <input type="text"
+               placeholder="Enter your reminder.."
+               v-model="todo.message">
+        <button class="btn secondary-white" v-on:click="add(todo.message)">Add</button>
+      </form>
+      <ul class="list-style-none margin-0 padding-0">
+        <li class="flex items-center margin-bottom-100"
+            v-for="todo in todos"
+            v-bind:class="{ 'opacity-50': todo.completed }">
+          <input type="checkbox"
+                 v-bind:checked="todo.completed"
+                 v-on:change="toggle(todo.id, todo.completed)">
+          <span class="font-size-18 margin-left-100 margin-right-100"
+                v-bind:class="{ 'text-decoration-line-through': todo.completed }">{{ todo.message }}</span>
+          <button class="trash" v-on:click="remove(todo.id)" v-html="icons.trash"></button>
+        </li>
+      </ul>
+      <p class="font-size-22 opacity-70" v-if="todos.length === 0">You have no reminders.</p>
+    </article>
 
   </div>
 </template>
